29e771e4e057ac3dd7d319ebf2a78d9d55783465
brunorafael8 <bruninho_lito@hotmail.com>
2024-06-27 06:31:45 -0300

    feat: create home page on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Census from "./pages/Census";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: "/censo",
     element: <Census />,
@@ -11,9 +15,27 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <>
+    <div className="bg-background">
+      <div className="flex items-center justify-between h-full space-y-8 p-8">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">
+            Bem Vindo, RD AL!
+          </h2>
+          <p className="text-muted-foreground">
+            ⁠"A consequência que o nosso comportamento gera, tende a determinar
+            a probabilidade deste comportamento ocorrer novamente."
+          </p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <img
+            src="https://avatars.githubusercontent.com/u/56189229?v=4"
+            alt="avatar"
+            className="w-10 h-10 rounded-full"
+          />
+        </div>
+      </div>
       <RouterProvider router={router} />
-    </>
+    </div>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,217 @@
+import * as React from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import "../global.css";
+import { Card } from "@/components/ui/card";
+
+// import { DataTableToolbar } from "./data-table-toolbar"
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+const DataTableColumnHeader = ({ column, title }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <span>{title}</span>
+    </div>
+  );
+};
+
+export const columns = [
+  {
+    accessorKey: "id",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Task" />
+    ),
+    cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "title",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Title" />
+    ),
+    cell: ({ row }) => <div className="w-[80px]">{row.getValue("tit")}</div>,
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Status" />
+    ),
+    cell: ({ row }) => <div className="w-[80px]">{row.getValue("status")}</div>,
+
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: "priority",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Priority" />
+    ),
+    cell: ({ row }) => (
+      <div className="w-[80px]">{row.getValue("priority")}</div>
+    ),
+
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+];
+
+const DATA_TABLE = [
+  {
+    id: "TASK-8782",
+    title:
+      "You can't compress the program without quantifying the open-source SSD pixel!",
+    status: "in progress",
+    label: "documentation",
+    priority: "medium",
+  },
+  {
+    id: "TASK-7878",
+    title:
+      "Try to calculate the EXE feed, maybe it will index the multi-byte pixel!",
+    status: "backlog",
+    label: "documentation",
+    priority: "medium",
+  },
+  {
+    id: "TASK-7839",
+    title: "We need to bypass the neural TCP card!",
+    status: "todo",
+    label: "bug",
+    priority: "high",
+  },
+  {
+    id: "TASK-5562",
+    title:
+      "The SAS interface is down, bypass the open-source pixel so we can back up the PNG bandwidth!",
+    status: "backlog",
+    label: "feature",
+    priority: "medium",
+  },
+  {
+    id: "TASK-8686",
+    title:
+      "I'll parse the wireless SSL protocol, that should driver the API panel!",
+    status: "canceled",
+    label: "feature",
+    priority: "medium",
+  },
+];
+
+function Home() {
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  const table = useReactTable({
+    data: DATA_TABLE,
+    columns,
+    state: {
+      sorting,
+      columnVisibility,
+      rowSelection,
+      columnFilters,
+    },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+  });
+
+  return (
+    <div className="space-y-4 h-full">
+      {/* <DataTableToolbar table={table} /> */}
+      <Card className="w-1/2">
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id} colSpan={header.colSpan}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default Home;
